2b73fb87946f0e15946d5d0cd1fa6f523cc030e0
wouterpleizier <wouterpleizier@planet.nl>
2024-04-07 15:57:55 +0200

    Implement scale factor and max width/height for saved captures

--- EarthRipperHook/Capture/CaptureWriter.cs
+++ EarthRipperHook/Capture/CaptureWriter.cs
@@ -11,50 +11,117 @@ namespace EarthRipperHook.Capture
     {
         internal static bool Write(CaptureTask captureTask, nuint qImage, nuint fileName, string format, int quality)
         {
-            return captureTask.Format switch
-            {
-                OutputFormat.UserSpecified => Original<QImage.Save>()(qImage, fileName, format, captureTask.Quality ?? quality),
-                OutputFormat.JPG => Original<QImage.Save>()(qImage, fileName, "JPG", captureTask.Quality ?? quality),
-                OutputFormat.PNG => Original<QImage.Save>()(qImage, fileName, "PNG", -1),
-                OutputFormat.PNG_Gray16 => Write16BitGrayscalePNG(qImage, fileName),
-                _ => throw new NotImplementedException()
-            };
-        }
-
-        private static bool Write16BitGrayscalePNG(nuint qImage, nuint fileName)
-        {
-            string path = new QString(fileName).ToString();
-            int width = Original<QImage.Width>()(qImage);
-            int height = Original<QImage.Height>()(qImage);
+            int renderWidth = Original<QImage.Width>()(qImage);
+            int renderHeight = Original<QImage.Height>()(qImage);
             int bytesPerLine = Original<QImage.BytesPerLine>()(qImage);
-            int bytesPerPixel = bytesPerLine / width;
-            int byteCount = bytesPerLine * height;
+            int bytesPerPixel = bytesPerLine / renderWidth;
+            int byteCount = bytesPerLine * renderHeight;
 
             nuint data = Original<QImage.ConstBits>()(qImage);
             Memory.CurrentProcess.ReadRaw(data, out byte[] bgraBytes, byteCount);
 
-            List<byte> grayscaleBytes = new List<byte>(width * height * sizeof(ushort));
-            for (int i = 0; i < byteCount; i += bytesPerPixel)
+            List<byte> bytes = [];
+            if (captureTask.Format == OutputFormat.PNG_Gray16)
             {
-                grayscaleBytes.Add(bgraBytes[i + 1]);
-                grayscaleBytes.Add(bgraBytes[i + 2]);
+                for (int i = 0; i < byteCount; i += bytesPerPixel)
+                {
+                    bytes.Add(bgraBytes[i + 1]);
+                    bytes.Add(bgraBytes[i + 2]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < byteCount; i += bytesPerPixel)
+                {
+                    bytes.Add(bgraBytes[i + 2]);
+                    bytes.Add(bgraBytes[i + 1]);
+                    bytes.Add(bgraBytes[i]);
+                }
             }
 
+            (PixelFormat pixelFormat, int stride) = captureTask.Format switch
+            {
+                OutputFormat.UserSpecified or OutputFormat.JPG or OutputFormat.PNG =>
+                    (PixelFormats.Rgb24,
+                    renderWidth * sizeof(byte) * 3),
+
+                OutputFormat.PNG_Gray16 =>
+                    (PixelFormats.Gray16,
+                    renderWidth * sizeof(byte) * 2),
+
+                _ => throw new NotImplementedException()
+            };
+
             BitmapSource bitmapSource = BitmapSource.Create(
-                pixelWidth: width, pixelHeight: height,
+                pixelWidth: renderWidth, pixelHeight: renderHeight,
                 dpiX: 96.0, dpiY: 96.0,
-                pixelFormat: PixelFormats.Gray16,
+                pixelFormat: pixelFormat,
                 palette: null,
-                pixels: grayscaleBytes.ToArray(),
-                stride: width * sizeof(ushort));
+                pixels: bytes.ToArray(),
+                stride: stride);
 
-            PngBitmapEncoder pngEncoder = new PngBitmapEncoder();
-            pngEncoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+            (int outputWidth, int outputHeight) = GetScaledOutputSize(captureTask, renderWidth, renderHeight);
+            if (outputWidth != renderWidth || outputHeight != renderHeight)
+            {
+                double scaleX = (double)outputWidth / renderWidth;
+                double scaleY = (double)outputHeight / renderHeight;
+                bitmapSource = new TransformedBitmap(bitmapSource, new ScaleTransform(scaleX, scaleY));
+            }
 
-            using FileStream fileStream = File.OpenWrite(path);
-            pngEncoder.Save(fileStream);
+            BitmapEncoder encoder;
+            if ((captureTask.Format is OutputFormat.UserSpecified && format == "JPG")
+                || captureTask.Format is OutputFormat.JPG)
+            {
+                encoder = new JpegBitmapEncoder() { QualityLevel = captureTask.Quality ?? quality };
+            }
+            else if ((captureTask.Format is OutputFormat.UserSpecified && format == "PNG")
+                || captureTask.Format is OutputFormat.PNG or OutputFormat.PNG_Gray16)
+            {
+                encoder = new PngBitmapEncoder();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+            string outputPath = new QString(fileName).ToString();
+            using FileStream fileStream = File.OpenWrite(outputPath);
+            
+            encoder.Save(fileStream);
 
             return true;
+        }
+
+        private static (int width, int height) GetScaledOutputSize(CaptureTask captureTask,
+                                                                   int renderWidth,
+                                                                   int renderHeight)
+        {
+            double width = renderWidth;
+            double height = renderHeight;
+
+            if (captureTask.ScaleFactor is not null and > 0.0 and < 1.0)
+            {
+                width *= captureTask.ScaleFactor.Value;
+                height *= captureTask.ScaleFactor.Value;
+            }
+
+            if (captureTask.MaxWidth is not null and > 0 && width > captureTask.MaxWidth)
+            {
+                double scaleFactor = captureTask.MaxWidth.Value / width;
+                width *= scaleFactor;
+                height *= scaleFactor;
+            }
+
+            if (captureTask.MaxHeight is not null and > 0 && height > captureTask.MaxHeight)
+            {
+                double scaleFactor = captureTask.MaxHeight.Value / height;
+                width *= scaleFactor;
+                height *= scaleFactor;
+            }
+
+            return (Convert.ToInt32(width), Convert.ToInt32(height));
         }
     }
 }
